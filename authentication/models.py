d68382ebefda017a673966d14e010809e21b1997
mahaab341 <mahaab341@gmail.com>
2025-10-29 20:46:59 -0700

    first commit

--- authentication/models.py
+++ authentication/models.py
@@ -14,7 +14,7 @@ class CustomUser(AbstractUser):
     phone_regex = RegexValidator(
         regex=r'^\+?1?\d{9,15}$',
         message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
-    )
+    )   
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
